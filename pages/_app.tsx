15b16914867f7baf7c3a84c84fbdad6272ee3fe9
Thiago Resende <thiago.gcresende@gmail.com>
2021-02-20 21:50:25 -0300

    add script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,20 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <GlobalStyles />
       <Component {...pageProps} />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-133675841-1"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-133675841-1');
+          `
+        }}
+      ></script>
     </>
   )
 }
