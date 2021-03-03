d93eb408dcf7bd063209a18858d3910936be250e
Thiago Resende <thiago.gcresende@gmail.com>
2021-03-03 19:32:51 -0300

    Social Component

---
+++ src/components/Social/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import SocialLink from 'components/SocialLink'
+import * as S from './styles'
+
+const Social = () => {
+  return (
+    <S.Wrapper>
+      <SocialLink url="https://github.com/tresende" name="github" />
+      <SocialLink
+        url="https://www.linkedin.com/in/thiago-resende-46175391"
+        name="linkedin"
+      />
+      <SocialLink url="mailto:thiago.gcresende@gmail.com" name="email" />
+      <SocialLink url="https://www.instagram.com/thiago.gcr" name="instagram" />
+      <SocialLink
+        url="https://www.facebook.com/thiagooooooooooooooo"
+        name="facebook"
+      />
+    </S.Wrapper>
+  )
+}
+export default Social
