157d2ca8b705cb64afa9c26024a41aeb3a2925dd
Thiago Resende <thiago.gcresende@gmail.com>
2021-03-03 19:33:26 -0300

    Change home

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,5 +1,9 @@
 import Head from 'next/head'
-import SocialLink from 'components/SocialLink'
+
+import Video from 'components/Video'
+import Terminal from 'components/Terminal'
+import Bio from 'components/Bio'
+import Social from 'components/Social'
 
 import * as S from './styles'
 
@@ -11,51 +15,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <S.Video preload="none" autoPlay muted loop>
-        <source
-          src="https://raw.githubusercontent.com/tresende/tresende.github.io/master/public/videos/bg.mp4"
-          type="video/mp4"
-        />
-      </S.Video>
+      <Video />
       <S.Main>
-        <S.Container className="flex">
-          <S.Terminal>
-            <S.Path>
-              ~/Projects/tresende.github.io <S.Branch>main*</S.Branch>
-            </S.Path>
-            <S.Cursor>❯ </S.Cursor>
-            <span>cat readme.txt</span>
-            <S.Block>_</S.Block>
-          </S.Terminal>
-          <S.Bio>
-            Hello, friend.
-            <br />
-            My name is Thiago Resende. I&apos;m a frontend developer living in
-            Belo Horizonte, Brazil. I&apos;m a technology fan 💾, programming
-            👨‍💻, and coffee ☕️. These are my high-level skills: <br />
-            - React <br />
-            - Angular <br />
-            - Node <br />
-            - Flutter <br />
-            - SNES <br />
-            - Play bass <br />
-          </S.Bio>
-          <S.SocialMedia>
-            <SocialLink url="https://github.com/tresende" name="github" />
-            <SocialLink
-              url="https://www.linkedin.com/in/thiago-resende-46175391"
-              name="linkedin"
-            />
-            <SocialLink url="mailto:thiago.gcresende@gmail.com" name="email" />
-            <SocialLink
-              url="https://www.instagram.com/thiago.gcr"
-              name="instagram"
-            />
-            <SocialLink
-              url="https://www.facebook.com/thiagooooooooooooooo"
-              name="facebook"
-            />
-          </S.SocialMedia>
+        <S.Container>
+          <Terminal />
+          <Bio />
+          <Social />
         </S.Container>
       </S.Main>
     </>
--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -1,13 +1,5 @@
 import styled from 'styled-components'
 
-export const Video = styled.video`
-  position: fixed;
-  left: 0;
-  top: 0;
-  min-width: 100%;
-  min-height: 100%;
-`
-
 export const Main = styled.main`
   height: 100%;
   width: 100%;
@@ -37,54 +29,4 @@ export const Container = styled.section`
     height: 100vh;
     padding: 2rem;
   }
-`
-
-export const InlineText = styled.div`
-  margin-bottom: 1rem;
-  white-space: nowrap;
-`
-
-export const Terminal = styled(InlineText)``
-
-export const Path = styled(InlineText)`
-  color: greenyellow;
-`
-
-export const Branch = styled.span`
-  color: gray;
-`
-
-export const Cursor = styled.span`
-  color: aqua;
-`
-
-export const Block = styled.span`
-  animation-name: blink;
-  animation-duration: 700ms;
-  animation-iteration-count: infinite;
-
-  @keyframes blink {
-    0% {
-      opacity: 1;
-    }
-
-    50% {
-      opacity: 0;
-    }
-
-    100% {
-      opacity: 1;
-    }
-  }
-`
-export const Bio = styled.p`
-  display: flex;
-  line-height: 2rem;
-`
-
-export const SocialMedia = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin: 0 auto;
-  width: 11rem;
 `
