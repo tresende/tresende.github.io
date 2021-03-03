4d5a1789ee103c03283b7dd3b0db9d57004b0ab2
Thiago Resende <thiago.gcresende@gmail.com>
2021-03-03 19:32:43 -0300

    Bio component

---
+++ src/components/Bio/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const Bio = () => {
+  return (
+    <S.Wrapper>
+      Hello, friend.
+      <br />
+      My name is Thiago Resende. I&apos;m a frontend developer living in Belo
+      Horizonte, Brazil. I&apos;m a technology fan 💾, programming 👨‍💻, and
+      coffee ☕️. These are my high-level skills: <br />
+      - React <br />
+      - Angular <br />
+      - Node <br />
+      - Flutter <br />
+      - SNES <br />
+      - Play bass <br />
+    </S.Wrapper>
+  )
+}
+export default Bio
---
+++ src/components/Bio/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.p`
+  display: flex;
+  line-height: 2rem;
+`
