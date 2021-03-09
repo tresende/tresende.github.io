c201dd3376142d12df7ec762d05d77e7e08bba86
Thiago Resende <thiago.gcresende@gmail.com>
2021-03-09 20:57:28 -0300

    removing media query

--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -8,9 +8,6 @@ export const Main = styled.main`
   justify-content: center;
   align-items: center;
   background-color: rgba(0, 0, 0, 0.6);
-  @media only screen and (max-width: 760px) {
-    background-color: rgba(0, 0, 0, 0.7);
-  }
 `
 
 export const Container = styled.section`
