afb6d1d3f12df4135c7312512bc4d5f4b44cdd56
Thiago Resende <thiago.gcresende@gmail.com>
2021-03-09 21:13:24 -0300

    rename folder Github

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -3,7 +3,7 @@ import Video from 'components/Video'
 import Terminal from 'components/Terminal'
 import Bio from 'components/Bio'
 import Social from 'components/Social'
-import Github from 'components/GithubCorner'
+import Github from 'components/Github'
 
 import * as S from './styles'
 
