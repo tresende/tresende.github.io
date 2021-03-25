e4f2d3e892baa13a2ce71d9577a356d5bf2af508
Thiago Resende <thiago.gcresende@gmail.com>
2021-03-24 21:38:14 -0300

    Update index.tsx

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -5,6 +5,9 @@ const HeadComponent = () => (
     <title>(2) Whatsapp</title>
     <link rel="icon" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="author" content="Thiago Resende" />
+    <meta name="description" content="Thiago's site" />
+    <meta name="keywords" content="React, nextjs, thiago resende, belo horizonte, frontend" />
   </Head>
 )
 
