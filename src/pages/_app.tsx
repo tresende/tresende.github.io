23bc4225ed54c6829388c6531b4f13c251a258c2
Thiago Resende <thiago.gcresende@gmail.com>
2021-08-16 22:11:44 -0300

    🔒️ Fix securty issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router'
 import { DefaultSeo } from 'next-seo'
 
 import Head from 'components/Head'
 import GlobalStyles from './global'
 import SEO from '../../next-seo.config'
+import { AppProps } from 'next/dist/shared/lib/router/router'
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
