9952361a9658b45e1f235aab2402a58f08289553
Thiago Resende <thiago.gcresende@gmail.com>
2021-08-29 17:47:42 -0300

    add mock api

--- src/pages/api/review.ts
+++ src/pages/api/review.ts
@@ -1,9 +1,12 @@
-import Mock from 'mocks/mock'
 import { NextApiRequest, NextApiResponse } from 'next'
+import request from 'request'
+
+import Mock from 'mocks/mock'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    res.send(req.body)
+    await request.post('https://borc-9faa2-default-rtdb.firebaseio.com/review.json', { json: req.body })
+    await res.send(req.body)
   } else {
     res.send(Mock)
   }
