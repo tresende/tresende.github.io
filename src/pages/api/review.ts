31e88183073cf141f8b6f7a86e25d011b301137f
Thiago Resende <thiago.cresende@gmail.com>
2021-08-29 21:56:57 -0300

    add cors suport

--- src/pages/api/review.ts
+++ src/pages/api/review.ts
@@ -1,9 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors'
 import request from 'request'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 import Mock from 'mocks/mock'
+import initMiddleware from '../../lib/init-middleware'
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  Cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS']
+  })
+)
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  await cors(req, res)
   if (req.method === 'POST') {
     await request.post('https://borc-9faa2-default-rtdb.firebaseio.com/review.json', { json: req.body })
     await res.send(req.body)
