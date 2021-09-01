0a14af156a12bec267f1c60317d0c790779d4462
Thiago Resende <thiago.cresende@gmail.com>
2021-09-01 14:51:25 -0300

    :bug: fix styledcomponents startup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,32 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import Analytics from 'components/Analytics'
+import { ServerStyleSheet } from 'styled-components'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
-class Document extends NextDocument {
+import Analytics from 'components/Analytics'
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
   render() {
     return (
       <Html lang="en">
@@ -15,5 +40,3 @@ class Document extends NextDocument {
     )
   }
 }
-
-export default Document
