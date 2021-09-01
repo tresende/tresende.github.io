72864a12c1c71346e4179bb03a806095469858ea
Thiago Resende <thiago.cresende@gmail.com>
2021-09-01 15:36:49 -0300

    🎨  change bg location

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -5,10 +5,7 @@ import * as S from './styles'
 const Bio = () => (
   <S.Wrapper preload="auto" autoPlay muted loop>
     <Image layout="fill" objectFit="cover" src="/img/fake-bg.png" alt={`busy avenue`} />
-    <source
-      src="https://raw.githubusercontent.com/tresende/tresende.github.io/master/public/videos/bg.mp4"
-      type="video/mp4"
-    />
+    <source src="/videos/bg.mp4" type="video/mp4" />
   </S.Wrapper>
 )
 
