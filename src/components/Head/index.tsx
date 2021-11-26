b8efea356e8bf1edead45bc52fbcc15c9a07d816
Thiago Resende <thiago.gcresende@gmail.com>
2021-11-26 09:14:13 -0300

    Update index.tsx

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -5,7 +5,7 @@ const HeadComponent = () => (
   <Head>
     <TagManager />
     <title>(2) Whatsapp</title>
-    <meta name="theme-color" content="#444444" />
+    <meta name="theme-color" content="#000000" />
     <link rel="icon" href="/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="author" content="Thiago Resende" />
