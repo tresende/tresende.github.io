528f0ba8435b0cd8356643673c65c7006817c226
Thiago Resende <thiago.gcresende@gmail.com>
2022-01-06 23:50:33 -0300

    ci: fix test import

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -2,11 +2,11 @@ import { render, screen } from '@testing-library/react'
 
 import Home from '.'
 
-jest.mock('../../components/video', () => () => <div data-testid="video" />)
-jest.mock('../../components/Terminal', () => () => <div data-testid="terminal" />)
-jest.mock('../../components/Bio', () => () => <div data-testid="bio" />)
-jest.mock('../../components/Social', () => () => <div data-testid="social" />)
-jest.mock('../../components/Github', () => () => <div data-testid="github" />)
+jest.mock('components/video', () => () => <div data-testid="video" />)
+jest.mock('components/Terminal', () => () => <div data-testid="terminal" />)
+jest.mock('components/Bio', () => () => <div data-testid="bio" />)
+jest.mock('components/Social', () => () => <div data-testid="social" />)
+jest.mock('components/Github', () => () => <div data-testid="github" />)
 
 describe('<Home />', () => {
   it('should render all components', () => {
