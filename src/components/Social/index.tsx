c8c12a3f3861125a901a1a8c405a46eafd7f79b7
Thiago Resende <thiago.resende@ifood.com.br>
2022-01-12 19:44:42 -0300

    chore(social): change instagram url

--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -7,7 +7,7 @@ const Social = () => (
     <SocialLink url="https://github.com/tresende" name="github" />
     <SocialLink url="https://www.linkedin.com/in/thiago-resende-46175391" name="linkedin" />
     <SocialLink url="mailto:thiago.gcresende@gmail.com" name="email" />
-    <SocialLink url="https://www.instagram.com/thiago.gcr" name="instagram" />
+    <SocialLink url="https://www.instagram.com/tresende.js" name="instagram" />
     <SocialLink url="https://www.facebook.com/thiagooooooooooooooo" name="facebook" />
   </S.Wrapper>
 )
