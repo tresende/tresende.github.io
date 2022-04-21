d2cdfe46f614447d4f1d165f64bdaf497b253f60
Thiago Resende <thiago.gcresende@gmail.com>
2022-04-21 19:15:13 -0300

    chore(deps): update all deps

--- styled-componentes.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
-import theme from 'styles/theme'
-
-type Theme = typeof theme
-declare module 'styled-components' {
-  export interface DefaultTheme extends Theme {}
-}
