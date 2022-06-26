7cc61882025cb537676dd1a8f3a7495d94d3b9b7
Thiago Resende <thiago.gcresende@gmail.com>
2022-06-26 19:08:50 -0300

    feat: Add save request in firestore

---
+++ src/utils/convetToFirestore.ts
@@ -0,0 +1,13 @@
+export const convetToFirestore = (object: any) => {
+  const keys = Object.keys(object).map((item) => ({
+    [item]: {
+      stringValue: object[item]
+    }
+  }))
+
+  let data = {}
+  keys.forEach((prop) => {
+    data = { ...prop, ...data }
+  })
+  return data
+}
