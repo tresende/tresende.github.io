86084d4a394f5d2fd4e8332d729dcb505ab6d26e
Thiago Resende <thiago.gcresende@gmail.com>
2022-07-23 19:20:23 -0300

    test: update snapshots

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import * as S from './styles'
 
 const Bio = () => (
-  <S.Wrapper preload="auto" autoPlay muted loop>
+  <S.Wrapper preload="auto" autoPlay muted loop playsInline>
     <Image layout="fill" objectFit="cover" src="/img/fake-bg.png" alt={`busy avenue`} />
     <source src="/videos/bg.mp4" type="video/mp4" />
   </S.Wrapper>
