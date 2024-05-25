7947e39eb1ed5ed6e9eaa15f17a51e7d2282834a
Thiago Resende <thiago.gcresende@gmail.com>
2024-05-25 20:01:24 -0300

    chore: update yarn.lock

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -4,16 +4,16 @@ import Experience from 'components/Experience'
 import Skills from 'components/Skills'
 import Title from 'components/Title'
 
-const Home = () => (
-  <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
-    <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
-      <Title />
-      <About />
-      <Experience />
-      <Education />
-      <Skills />
-    </section>
-  </main>
-)
-
-export default Home
+export default function Home() {
+  return (
+    <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
+      <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
+        <Title />
+        <About />
+        <Experience />
+        <Education />
+        <Skills />
+      </section>
+    </main>
+  )
+}
