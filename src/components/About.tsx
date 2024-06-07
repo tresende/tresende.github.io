6d07f0b01119eb699232b33da7b7e13199189157
Thiago Resende <thiago.gcresende@gmail.com>
2024-06-07 20:47:45 -0300

    Update About and Title components with improved descriptions

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,9 +3,9 @@ export default function About() {
     <section className="flex min-h-0 flex-col gap-y-3">
       <h2 className="text-xl font-bold">About</h2>
       <p className="text-pretty font-mono text-sm text-muted-foreground">
-        With over 13 years of software development experience, I am a senior software engineer passionate about creating
-        innovative and highly scalable solutions. My goal is to leverage my software engineering skills to deliver
-        high-quality, high-performance, and easy-to-use products.
+        With over {new Date().getFullYear() - 2011} years of software development experience, I am a senior software
+        engineer passionate about creating innovative and highly scalable solutions. My goal is to leverage my software
+        engineering skills to deliver high-quality, high-performance, and easy-to-use products.
       </p>
       <p className="text-pretty font-mono text-sm text-muted-foreground">
         I am currently working on a high-traffic application using Java, Kotlin, and microservices. This project
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -16,28 +16,13 @@ export default function Title() {
               target="_blank"
               rel="noreferrer"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-globe size-3"
-              >
-                <circle cx="12" cy="12" r="10"></circle>
-                <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"></path>
-                <path d="M2 12h20"></path>
-              </svg>
-              Belo Horizonte, Minas Gerais, Brazil
+              <span className="hidden sm:block">🌏</span>
+              <span>Belo Horizonte, Minas Gerais, Brazil</span>
             </a>
           </p>
           <Social />
         </div>
-        <span className="relative flex shrink-0 overflow-hidden rounded-xl size-28">
+        <span className="relative flex shrink-0 overflow-hidden rounded-xl size-20 sm:size-28">
           <img className="aspect-square h-full w-full" alt="Thiago Resende" src="https://github.com/tresende.png" />
         </span>
       </div>
