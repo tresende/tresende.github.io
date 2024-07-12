15b7f7b2d5017fa4617e77e055711bfdc6281c75
Thiago Resende <thiago.gcresende@gmail.com>
2024-07-12 20:08:47 -0300

    chore: Update meta keywords in Head component

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -10,7 +10,10 @@ export default function HeadComponent() {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="author" content="Thiago Resende" />
       <meta name="description" content="Thiago's site" />
-      <meta name="keywords" content="React, nextjs, thiago resende, belo horizonte, frontend" />
+      <meta
+        name="keywords"
+        content="Java, Javascript, typescript, node thiago resende, belo horizonte, software engineer"
+      />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
