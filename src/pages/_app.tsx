a1c342f0e8d417d479c72c4cdb3ee4c626961c8d
Thiago Resende <thiago.gcresende@gmail.com>
2025-10-25 18:52:25 -0300

    refactor(app): update next-seo integration and upgrade dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import '../styles/global.css'
 import Head from 'components/Head'
 import { AppProps } from 'next/dist/shared/lib/router/router'
-import { DefaultSeo } from 'next-seo'
+import { generateNextSeo } from 'next-seo/pages'
 
 import SEO from '../../next-seo.config'
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     <Head />
-    <DefaultSeo {...SEO} />
+    {generateNextSeo(SEO)}
     <Component {...pageProps} />
   </>
 )
